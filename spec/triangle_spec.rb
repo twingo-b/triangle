58c8e1c5f13176f0748311b5f5ed3a07d62ec4be
twingo-b <twingo.b@gmail.com>
2016-05-17 22:29:36 +0900

    一通り必要な機能を実装した

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../triangle')
 
-describe 'Triangle' do
-  it 'first test'
+describe Triangle do
+  describe 'pre_condition?' do
+    specify { expect(Triangle.new([0, 0, 0]).pre_condition?).to be_falsey }
+    specify { expect(Triangle.new([nil, 1, 1]).pre_condition?).to be_falsey }
+  end
+
+  describe 'is_triangle?' do
+    specify { expect(Triangle.new([2, 1, 1]).is_triangle?).to be_falsey }
+  end
+
+  describe 'is_equilateral_triangle?' do
+    specify { expect(Triangle.new([1, 1, 1]).is_equilateral_triangle?).to be_truthy }
+  end
+
+  describe 'is_isosceles_triangle?' do
+    specify { expect(Triangle.new([2, 2, 1]).is_isosceles_triangle?).to be_truthy }
+  end
+
+  describe 'is_scalene_triangle?' do
+    specify { expect(Triangle.new([5, 4, 3]).is_scalene_triangle?).to be_truthy }
+  end
+
+  describe 'detect' do
+    specify { expect{Triangle.new([0, 0, 0]).detect}.to output("三角形じゃないです＞＜\n").to_stdout }
+    specify { expect{Triangle.new([nil, 1, 1]).detect}.to output("三角形じゃないです＞＜\n").to_stdout }
+    specify { expect{Triangle.new([2, 1, 1]).detect}.to output("三角形じゃないです＞＜\n").to_stdout }
+    specify { expect{Triangle.new([1, 1, 1]).detect}.to output("正三角形ですね！\n").to_stdout }
+    specify { expect{Triangle.new([2, 2, 1]).detect}.to output("二等辺三角形ですね！\n").to_stdout }
+    specify { expect{Triangle.new([5, 4, 3]).detect}.to output("不等辺三角形ですね！\n").to_stdout }
+  end
 end
--- triangle.rb
+++ triangle.rb
@@ -1,3 +1,46 @@
 class Triangle
+  def initialize(argv)
+    @args_a = argv[0]
+    @args_b = argv[1]
+    @args_c = argv[2]
+  end
 
+  def pre_condition?
+    [@args_a, @args_b, @args_c].find_all{ |n| n.is_a?(Numeric) && (n > 0) }.length == 3
+  end
+
+  # 三角不等式: 三角形の 2 辺の長さの和は残りの 1 辺の長さよりも大きい
+  # https://ja.wikipedia.org/wiki/%E4%B8%89%E8%A7%92%E4%B8%8D%E7%AD%89%E5%BC%8F
+  def is_triangle?
+    pre_condition? && @args_a < @args_b + @args_c && @args_b < @args_c + @args_a && @args_c < @args_a + @args_b
+  end
+
+  def is_equilateral_triangle?
+    @args_a == @args_b && @args_b == @args_c && @args_c == @args_a
+  end
+
+  def is_isosceles_triangle?
+    @args_a == @args_b || @args_b == @args_c || @args_c == @args_a
+  end
+
+  def is_scalene_triangle?
+    !is_isosceles_triangle?
+  end
+
+  def detect
+    if !is_triangle?
+      puts '三角形じゃないです＞＜'
+    elsif is_equilateral_triangle?
+      puts '正三角形ですね！'
+    elsif is_isosceles_triangle?
+      puts '二等辺三角形ですね！'
+    elsif is_scalene_triangle?
+      puts '不等辺三角形ですね！'
+    end
+  end
+
+end
+
+if __FILE__ == $0
+  Triangle.new(ARGV.map{ |s| s.gsub(/,$/,'').to_i }).detect
 end
